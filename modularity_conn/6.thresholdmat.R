2620843cb0f9b8ed2616ad70c6b3849be3fc589c
michaelandric <andric.michael@gmail.com>
2012-11-20 14:13:32 +0100

    updated

--- modularity_conn/6.thresholdmat.R
+++ modularity_conn/6.thresholdmat.R
@@ -1,5 +1,4 @@
-library(bct)
-
+## This is to threshold the N x N connectivity matrix
 Args <- Sys.getenv("R_ARGS")
 print(noquote(strsplit(Args," ")[[1]]))
 print(length(noquote(strsplit(Args," ")[[1]])))
@@ -7,12 +6,7 @@ subj <- noquote(strsplit(Args," ")[[1]][1])
 cond <- as.numeric(noquote(strsplit(Args," ")[[1]][2]))
 
 
-thresh_func <- function(i,ss)
-{
-    library(bct)
-    corrmat <- paste(Sys.getenv("state"),"/",ss,"/corrTRIM_BLUR/cleanTS.Cond",i,".",subj,".noijk_dump.bin.corr",sep="")
-    fthreshold_absolute(corrmat,.5,1)
-}
+library(bct)
+corrmat <- paste(Sys.getenv("state"),"/",subj,"/corrTRIM_BLUR/cleanTS.",cond,".",subj,"_graymask_dump.bin.corr",sep="")
+fthreshold_absolute(corrmat,.5,1)
 
-
-thresh_func(cond,subj)
