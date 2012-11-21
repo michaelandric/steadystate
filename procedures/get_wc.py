6513e8061bdafc0f90f4db9ba33765d423aca8c9
michaelandric <andric.michael@gmail.com>
2012-11-21 19:36:30 +0100

    cleaned up code. now print dictionary

--- procedures/get_wc.py
+++ procedures/get_wc.py
@@ -5,7 +5,7 @@ import os
 
 def getwc():
     try:
-        fname = os.environ["state"]+"/"+ss+"/blur.4."+ss+".steadystate.TRIM.noijk_dump"
+        fname = os.environ["state"]+"/"+ss+"/masking/ijk_coords_graymattermask_"+ss
         textf = open(fname, 'r')
     except IOError:
         print "cannot open file %s for reading" % fname
@@ -13,18 +13,21 @@ def getwc():
         sys.exit(0)
 
     num_lines = sum(1 for line in open(fname))
-    return ss+" "+`num_lines`+"\n"
+    return num_lines
 
 
+subjects = ["MYTP","TRCO","PIGL","SNNW","LDMW","FLTM","EEPA","DNLN","CRFO","ANMS","BARS","MRZM","MRVV","MRMK","MRMC","MRAG","MNGO","LRVN","CLFR","ANGO"]
 
-
-subjects = ["ANGO","MYTP","CLFR","TRCO","PIGL","SNNW","LDMW","FLTM","EZCR","EEPA","DNLN","CRFO","ANMS","BARS","MRZM","MRVV","MRMK","MRMC","MRAG","MNGO","LRVN"]
 
 cntr = ""
+nvox = []
 for ss in subjects:
-    cntr += getwc()
+    cntr += ss+" "+`getwc()`+"\n"
+    nvox.append(getwc())
 
-outf = open('subject_blurTSwordcounts.txt','w')
+outf = open('subject_graymattermask_voxnum.txt','w')
 outf.write(cntr)
 outf.close()
 
+nvox_dict = dict(zip(subjects,nvox))
+print nvox_dict
