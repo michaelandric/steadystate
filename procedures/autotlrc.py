8ab242fe2725ceaa4779d4c1b099a31fe269a00c
michaelandric <andric.michael@gmail.com>
2012-11-21 19:41:49 +0100

    cleaned up code

--- procedures/autotlrc.py
+++ procedures/autotlrc.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 
 import os
+from subprocess import call
 from optparse import OptionParser
 
 class AutoTLRC:
 
     def get_opts(self):
-        desc = """simple program for doing trimming the time series"""
+        desc = """
+        simple program for doing trimming the time series
+        """
         self.usage = "usage: %prog [options]"
-        self.parser = OptionParser(description=desc, version="%prog 10.October.2012")
+        self.parser = OptionParser(description=desc, version="%prog Nov.2012")
         self.parser.add_option("--tlrc_brain", dest="tlrcT1",
                                help="specify the talairach anat")
         self.parser.add_option("--input", dest="input",
@@ -19,16 +22,26 @@ class AutoTLRC:
         (self.options, args) = self.parser.parse_args()
 
     def auto_tlrc_ex(self):
+        """
+        This is the main function that iterates across the 4 conditions
+        """
         os.chdir(os.environ["state"]+"/"+self.options.subject+"/corrTRIM_BLUR/")
         for cc in range(1,5):
-            print os.system("@auto_tlrc -apar "+self.options.tlrcT1+" -input "+self.options.subject+"."+`cc`+".degrees.ijkSHORT+orig -dxyz 2")
+            print call("@auto_tlrc -apar "+self.options.tlrcT1+" -input "+self.options.subject+"."+`cc`+".degrees_gray+orig -dxyz 2", shell=True)
 
     def index_auto_tlrc(self):
+        """
+        This was used to get index values, i.e., correspond voxel number in orig to tlrc space.
+        """
         os.chdir(os.environ["state"]+"/"+self.options.subject)
-        print os.system("@auto_tlrc -apar corrTRIM_BLUR/"+self.options.tlrcT1+" -input "+self.options.subject+"voxel_index.ijk+orig -dxyz 2")
+        print call("@auto_tlrc -apar corrTRIM_BLUR/"+self.options.tlrcT1+" -input "+self.options.subject+"voxel_index.ijk+orig -dxyz 2", shell=True)
 
 
-atlrc = AutoTLRC()
-atlrc.get_opts()
-atlrc.index_auto_tlrc()
+def main():
+    atlrc = AutoTLRC()
+    atlrc.get_opts()
+    atlrc.auto_tlrc_ex()
+
+if __name__ == "__main__":
+    main()
 
