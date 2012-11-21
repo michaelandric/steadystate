57030b47e3592b0e7bab9352c28ef741e6833a61
michaelandric <andric.michael@gmail.com>
2012-11-21 14:51:37 +0100

    cleaned this up a lot

--- procedures/exec_makesubmitargs.py
+++ procedures/exec_makesubmitargs.py
@@ -8,12 +8,12 @@ where 'submit.try' is the condor_submit file that you're adding arguments to
 import commands
 import makesubmitargs
 
-subjects = ["ANGO"]
+#subjects = ["ANGO"]
 #subjects = ["MYTP","TRCO","CLFR","PIGL","SNNW"]
 #subjects = ["LDMW","FLTM","EZCR","EEPA","DNLN","CRFO","ANMS","BARS"]
 #subjects = ["MRZM","MRVV","MRMK","MRMC","MRAG","MNGO","LRVN"]
 ##subjects = ["MYTP","TRCO","PIGL","SNNW","LDMW","FLTM","EZCR","EEPA","DNLN","CRFO","ANMS","BARS","MRZM","MRVV","MRMK","MRMC","MRAG","MNGO","LRVN"] #CLFR & ANGO not in here
-#subjects = ["MYTP","TRCO","PIGL","SNNW","LDMW","FLTM","EZCR","EEPA","DNLN","CRFO","ANMS","BARS","MRZM","MRVV","MRMK","MRMC","MRAG","MNGO","LRVN","CLFR"] # ANGO not in here
+subjects = ["MYTP","TRCO","PIGL","SNNW","LDMW","FLTM","EEPA","DNLN","CRFO","ANMS","BARS","MRZM","MRVV","MRMK","MRMC","MRAG","MNGO","LRVN","CLFR"] # ANGO & EZCR not in here
 conditions = range(1,5)
 
 """
@@ -26,7 +26,12 @@ this dictionary gives the highest level (at .5 corr threshold)
 """
 #dictionary2 = {'MYTP':(4,3,3,3), 'TRCO':(4,4,4,3), 'CLFR':(4,3,4,3), 'PIGL':(4,4,4,4), 'SNNW':(3,4,3,3)}
 #dictionary2 = {"LDMW":(,) ,"FLTM":() ,"EZCR":() ,"EEPA":() ,"DNLN":() ,"CRFO":() ,"ANMS":() ,"BARS":()}
-dictionary2 = {"ANGO":(3,3,3,2)}
+"""
+'tree_d' is a dictionary with the highest hierarachial levels.
+This dictionary was generated with 'get_level.py'
+CURRENTLY DOES NOT INCLUDE 'EZCR' - COULDN'T GET SEGMENTATION FOR MASK WITH THIS SS
+"""
+tree_d = {'BARS': (2, 2, 2, 2), 'FLTM': (3, 3, 2, 2), 'MRZM': (2, 2, 3, 2), 'ANMS': (2, 2, 2, 2), 'MRAG': (2, 2, 2, 3), 'ANGO': (3, 3, 3, 2), 'PIGL': (3, 3, 3, 3), 'MRMK': (2, 2, 2, 2), 'CRFO': (2, 3, 2, 2), 'EEPA': (3, 2, 3, 2), 'TRCO': (2, 2, 3, 3), 'MRMC': (2, 2, 2, 2), 'SNNW': (3, 2, 2, 3), 'LDMW': (2, 3, 2, 2), 'LRVN': (2, 2, 2, 2), 'MRVV': (3, 3, 2, 3), 'DNLN': (2, 2, 1, 3), 'CLFR': (3, 2, 3, 2), 'MYTP': (3, 3, 3, 3), 'MNGO': (2, 3, 3, 3)}
 hemispheres = ["lh","rh"]
 
 
@@ -35,30 +40,22 @@ mm = makesubmitargs.makeargs
 #mm.ijkTALAIRACHcoordsargs("TTavg152T1","automask_d1")
 
 for ss in subjects:
-    #print commands.getoutput("python makesubmitargs.py --subject "+ss) ## for dir maker
     #mm.ijkcoordsargs(ss)
-    #print commands.getoutput("python makesubmitargs.py --subject "+ss+" --arg1 /mnt/tier2/urihas/Andric/steadystate/ --arg2 automask_d3") ## for submit.10.getijk
     #mm.autotlrcargs(ss)
     #mm.voxel_id_args(ss, dictionary[ss])
     #mm.maskdumpargs(ss)
     #mm.maskmakerargs(ss)
     for cc in conditions:
         #for i in range(dictionary2[ss][cc-1]+1): 
-        #print commands.getoutput("python makesubmitargs.py --subject "+ss+" --arg1 "+`cc`) ## this was for submit.splice
         #mm.maskdumpargs(ss,cc)
         #mm.fcorrargs(ss,cc)
-        #print commands.getoutput("python makesubmitargs.py --subject "+ss+" --arg1 /mnt/tier2/urihas/Andric/steadystate/ --arg2 automask_d3 --arg3 "+`cc`) ## for submit.3.maskdump
-        #print commands.getoutput("python makesubmitargs.py --subject "+ss+" --arg1 "+dictionary[ss]+" --arg2 "+`cc`)  ## this is for submit.4.corr
         #mm.threshargs(ss,cc) ## for submit.6.threshold
         #mm.convertargs(ss,cc) ## for submit.7.blondel_convert
         #mm.blondelargs(ss,cc) ## for submit.8.blondel
-        #print commands.getoutput("python makesubmitargs.py --subject "+ss+" --arg1 "+`cc`)  ## for submit.6.threshold, submit.7.blondel_convert, submit.8.blondel, submit.9.hierarchy
-            #print commands.getoutput("python makesubmitargs.py --subject "+ss+" --arg1 /mnt/tier2/urihas/Andric/steadystate/ --arg2 "+`cc`+" --arg3 "+`i`) ## for submit.11.undump
-        mm.undumpargs(ss,cc,dictionary2[ss][cc-1])
+        mm.hierarchyargs(ss,cc,tree_d[ss][cc-1])
+        #mm.undumpargs(ss,cc,dictionary2[ss][cc-1])
         #mm.filter(ss,cc,dictionary2[ss][cc-1]) ## for submit.13.filter
         #mm.undump14(ss,cc,dictionary2[ss][cc-1],1)
         #mm.degree(ss,dictionary[ss],cc)
-        #for h in hemispheres:
-        #    mm.vol2surfargs(ss, "s"+ss+"_"+h+"_mesh140"+ss+"_"+h+".spec", "s"+ss+"_"+h+"_mesh140"+h+".smoothwm.asc", "s"+ss+"_"+h+"_mesh140"+h+".pial.asc", "max_abs", ss+"."+`cc`+".degrees.ijk+orig.", ss+"."+`cc`+".degrees_"+h+"_max_abs.1D")
-        #mm.bintomatrix_args(ss,dictionary[ss],cc)
+
 
