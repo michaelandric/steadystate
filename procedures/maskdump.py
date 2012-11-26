cab85f619ae7f17330312a96e80782a57ee31c3b
michaelandric <andric.michael@gmail.com>
2012-11-26 15:17:57 +0100

    cleaned up code

--- procedures/maskdump.py
+++ procedures/maskdump.py
@@ -1,32 +1,39 @@
 #!/usr/bin/python
 
 import os
+from subprocess import call
 from optparse import OptionParser
+from adwarp_mask import adwarp_call
 
 class Maskdump:
 
     def get_opts(self):
         desc = """simple program for doing 3dmaskdump"""
         self.usage = "usage: %prog [options]"
-        self.parser = OptionParser(description=desc, version="%prog Dec.2010")
+        self.parser = OptionParser(description=desc, version="%prog Nov.2012")
         self.parser.add_option("--ijk", dest="ijk",
                                help="specify 'yes' or 'no' whether to use 'ijk'")
-        self.parser.add_option("--automask", dest="amask",
-                               help="automask file")
+        self.parser.add_option("--mask", dest="inputmask",
+                               help="mask to be used")
         self.parser.add_option("--inputfile", dest="input",
                                help="input file")
         self.parser.add_option("--outputname", dest="outname",
                                help="output name")
+        self.parser.add_option("--subject", dest="subject",
+                               help="Subject identifier")
 
         (self.options, args) = self.parser.parse_args()
         
 
     def dump(self):
-        print os.system("3dmaskdump -mask "+self.options.amask+" "+self.options.input+" > "+self.options.outname)
+        print call("3dmaskdump -mask "+self.options.inputmask+" "+self.options.input+" > "+self.options.outname, shell=True)
 
 
+def main():
+    Md = Maskdump()
+    Md.get_opts()
+    adwarp_call(Md.options.subject)
+    Md.dump()
 
-Md = Maskdump()
-Md.get_opts()
-Md.dump()
-
+if __name__ == "__main__":
+    main()
