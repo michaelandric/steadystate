fda96d45a4f84685d43f2196721ab36351fe068c
michaelandric <andric.michael@gmail.com>
2012-11-26 15:18:55 +0100

    tlrcs the index vals

--- procedures/autotlrc.py
+++ procedures/autotlrc.py
@@ -29,18 +29,28 @@ class AutoTLRC:
         for cc in range(1,5):
             print call("@auto_tlrc -apar "+self.options.tlrcT1+" -input "+self.options.subject+"."+`cc`+".degrees_gray+orig -dxyz 2", shell=True)
 
-    def index_auto_tlrc(self):
+    def index_auto_tlrc_ex(self):
         """
         This was used to get index values, i.e., correspond voxel number in orig to tlrc space.
         """
-        os.chdir(os.environ["state"]+"/"+self.options.subject)
-        print call("@auto_tlrc -apar corrTRIM_BLUR/"+self.options.tlrcT1+" -input "+self.options.subject+"voxel_index.ijk+orig -dxyz 2", shell=True)
+        os.chdir(os.environ["state"]+"/"+self.options.subject+"/masking/")
+        apar = os.environ["state"]+"/"+self.options.subject+"/corrTRIM_BLUR/"+self.options.tlrcT1
+        input = self.options.subject+"graymatter_voxel_index.ijk+orig"
+        print call("@auto_tlrc -apar "+apar+" -input "+input+" -dxyz 2", shell=True)
+
+    def mask_auto_tlrc_ex(self):
+        """
+        This is used to talairach the graymatter mask
+        """
+        os.chdir(os.environ["state"]+"/"+self.options.subject+"/masking/")
+        graymask = self.options.subject+"_graymattermask+orig"
+        print call("@auto_tlrc -apar "+self.options.tlrcT1+" -input "+graymask+" -dxyz 2", shell=True)
 
 
 def main():
     atlrc = AutoTLRC()
     atlrc.get_opts()
-    atlrc.auto_tlrc_ex()
+    atlrc.index_auto_tlrc_ex()
 
 if __name__ == "__main__":
     main()
