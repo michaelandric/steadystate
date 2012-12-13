dfbe22bc169866576ad536e7a7e767a11553cac9
michaelandric <andric.michael@gmail.com>
2012-12-13 16:56:09 +0100

    used like exec_makesubmitargs. initial commit

---
+++ procedures/batch_args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import os
+import makesubmitargs
+
+batch = 10000
+totalvox = 231203
+starts = range(0,totalvox,batch)
+ends = []
+for i in starts:
+    ends.append(i+(batch-1))
+
+ends[len(ends)-1] = totalvox-1
+
+mm = makesubmitargs.makeargs
+
+for cc in range(1,5):
+    for i in range(0,len(starts)):
+        mm.major_noderoleargs(starts[i],ends[i],cc)
+
