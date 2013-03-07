c3b2ed37309c98b60fff92b9f9c8794548528612
michaelandric <andric.michael@gmail.com>
2013-03-07 18:34:34 +0100

    added linear test filter

--- modularity_conn/33.freqPowerExp.R
+++ modularity_conn/33.freqPowerExp.R
@@ -1,5 +1,4 @@
 ## Analysis to get the power law exponent
-library(igraph)
 Args <- Sys.getenv("R_ARGS")
 print(noquote(strsplit(Args," ")[[1]]))
 print(length(noquote(strsplit(Args," ")[[1]])))
@@ -7,17 +6,22 @@ Subj <- paste(noquote(strsplit(Args," ")[[1]][1]))
 Cond <- as.numeric(noquote(strsplit(Args," ")[[1]][2]))
 Tree <- as.numeric(noquote(strsplit(Args," ")[[1]][3]))
 
-spec_func <- function(x)
+linear_predictor <- seq(14)
+spec_func_linear_filt <- function(x)
 {
     voxts = ts(as.numeric(x), frequency=2/3)
     spec_out = spectrum(voxts, plot=F)
-    return(spec_out$spec[1:15])
+    usable_spec = spec_out$spec[2:15]
+    #linear_test_F = summary(lm(usable_spec ~ linear_predictor))$fstatistic[[1]]  ##Unused here. Left in code for use another time.
+    linear_pval = summary(lm(usable_spec ~ linear_predictor))$coefficients[8]
+    out <- c(usable_spec, round(linear_pval,4))
+    return(out)
 }
+
 
 freq_vals <- spectrum(ts(rnorm(90,1),frequency=2/3), plot=F)$freq
 
 setwd(paste(Sys.getenv("state"),"/",Subj,"/corrTRIM_BLUR/",sep=""))
-#print(getwd())
 ##These 'clean' time series were generated from 4.fcorr.py
 myts <- read.table(paste("cleanTS.",Cond,".",Subj,"_graymask_dump",sep=""))
 nvox <- dim(myts)[1]
@@ -29,14 +33,29 @@ mod_list <- unique(mods)
 
 for (i in mod_list)
 {
-    mod_vox <- which(mods==i)
-    spec_power <- rowMeans(apply(myts[mod_vox,], 1, spec_func))
-    power_exp <- coef(lm(log(spec_power) ~ log(1/freq_vals[1:15])))[[2]]
-    power_exp2 <- coef(lm(log(spec_power[2:15]) ~ log(1/freq_vals[2:15])))[[2]]
-    print(power.law.fit(spec_power))
-    print(coef(power.law.fit(spec_power))[[1]])
-    power_expalpha <- coef(power.law.fit(spec_power))[[1]]
-    output <- rbind(output, c(i, round(power_exp,3), round(power_exp2,3), round(power_expalpha,3)))
+    mod_vox <- which(mods==i) ## use only voxels belonging to the module
+    spec_apply <- apply(myts[mod_vox,], 1, spec_func_linear_filt) ## use the function on the module's voxels
+    if (length(as.matrix(which(spec_apply[15,] <= .05))) == 0)
+    {
+        spec_pval_filt = NA
+        num_survive_voxels = 0
+        proportion_survive_voxels = 0
+        power_exp = NA
+    }else
+    {
+        spec_pval_filt <- as.matrix(which(spec_apply[15,] > .05)) ## filter by those that don't show significant linear trend
+        num_survive_voxels <- length(mod_vox) - length(spec_pval_filt) ## find number of voxels that pass the filter
+        if (num_survive_voxels==1)
+        {
+            spec_power <- spec_apply[1:14,]
+        }else
+        {
+            spec_power <- rowMeans(spec_apply[1:14,-spec_pval_filt]) ## determine the spectrum power only using unfilterd
+        }
+        proportion_survive_voxels <- num_survive_voxels / length(mod_vox) ## find the proportion voxels that pass the filter
+        power_exp <- coef(lm(log(spec_power) ~ log(1/freq_vals[2:15])))[[2]] ## determine power exponent
+    }
+    output <- rbind(output, c(i, round(power_exp,3), length(spec_pval_filt), num_survive_voxels, round(proportion_survive_voxels,3))) 
 }
 
-write.table(output, paste("modules.",Cond,".",Subj,"_powerexp",sep=""), row.names=F,col.names=F,quote=F)
+write.table(output, paste("modules.",Cond,".",Subj,"_powerexp_filtered",sep=""), row.names=F,col.names=F,quote=F)
