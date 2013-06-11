9089f8359c7450651cc2834ce1b856e0a9e8a8cb
michaelandric <andric.michael@gmail.com>
2013-06-11 17:37:45 +0200

    test normality. parametic and non-parametric tests

--- modularity_conn/47.distance_stats.R
+++ modularity_conn/47.distance_stats.R
@@ -2,20 +2,22 @@
 subjects <- c("ANGO","CLFR","MYTP","TRCO","PIGL","SNNW","LDMW","FLTM","EEPA","DNLN","CRFO","ANMS","MRZM","MRVV","MRMK","MRMC","MRAG","MNGO","LRVN")
 conditions <- seq(4)
 
-dist_data <- c()
-subject_ids <- c()
+avg_dist_data <- c()
 
 for (ss in subjects)
 {
     for (i in conditions)
     {
         dist_scores <- paste("/mnt/tier2/urihas/Andric/steadystate/",ss,"/corrTRIM_BLUR/distance_",ss,"_Cond",i,".txt",sep="")
-        avg_dist_data <- c(avg_dist_data, mean(dist_scores))
+        avg_dist_data <- c(avg_dist_data, mean(as.matrix(read.table(dist_scores))))
     }
 }
 
 setwd("/mnt/tier2/urihas/Andric/steadystate/groupstats/")
 avg_dist_mat <- matrix(c(avg_dist_data), ncol=4, byrow=T)
+dist_df <- data.frame(avg_dist_data, rep(seq(4), 19), rep(subjects, each=4))
+colnames(dist_df) <- c("dists", "condition", "subject")
+print(summary(aov(dists ~ condition + Error(subject/condition), data=dist_df)))
 print(avg_dist_mat)
 print(colMeans(avg_dist_mat))
 print(apply(avg_dist_mat, 2, median))
