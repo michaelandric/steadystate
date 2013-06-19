575f830b71b124157dbd113312aeec8b3bf679b9
Michael Andric <andric.michael@gmail.com>
2013-06-19 14:18:08 +0200

    added exponential trace

--- modularity_conn/48.distance_pwr_func.R
+++ modularity_conn/48.distance_pwr_func.R
@@ -66,6 +66,7 @@ dat1 = dat1[which(dat1 >= cutoff)]
 tmp1 = hist(dat1, breaks = c(cutoff:(max(dat1) + 1)), plot = F)
 cum.dist1 = 1-cumsum(tmp1$counts) / length(dat1)
 d1 = fitting(dat1, (max(dat1) + 1))
+exp.trace1 <- exp(-d1$mu * (0:(max(dat1)+1)))
 gamma.trace1 <- 1 - pgamma((0:(max(dat1) + 1)), shape = d1$alpha, scale = d1$beta)
    
 
@@ -82,6 +83,7 @@ dat2 = dat2[which(dat2 >= cutoff)]
 tmp2 = hist(dat2, breaks = c(cutoff:(max(dat2) + 1)), plot=F)
 cum.dist2 = 1 - cumsum(tmp2$counts) / length(dat2)
 d2 = fitting(dat2, (max(dat2) + 1)) 
+exp.trace2 <- exp(-d2$mu * (0:(max(dat2)+1)))
 gamma.trace2 <- 1 - pgamma((0:(max(dat2)+1)), shape = d2$alpha, scale=d2$beta)
 
 
@@ -98,6 +100,7 @@ dat3 = dat3[which(dat3 >= cutoff)]
 tmp3 = hist(dat3, breaks = c(cutoff:(max(dat3) + 1)), plot=F)
 cum.dist3 = 1 - cumsum(tmp3$counts) / length(dat3)
 d3 = fitting(dat3, (max(dat3) + 1))
+exp.trace3 <- exp(-d3$mu * (0:(max(dat3)+1)))
 gamma.trace3 <- 1 - pgamma((0:(max(dat3) + 1)), shape=d3$alpha, scale=d3$beta)
 
 
@@ -114,6 +117,7 @@ dat4 = dat4[which(dat4 >= cutoff)]
 tmp4 = hist(dat4, breaks = c(cutoff:(max(dat4) + 1)), plot=F)
 cum.dist4 = 1 - cumsum(tmp4$counts) / length(dat4)
 d4 = fitting(dat4, (max(dat4) + 1))
+exp.trace4 <- exp(-d4$mu * (0:(max(dat4)+1)))
 gamma.trace4 <- 1 - pgamma((0:(max(dat4) + 1)), shape=d4$alpha, scale=d4$beta)
 
 pdf("distanceFilter6_log10fits.pdf")
@@ -123,3 +127,10 @@ lines(log10(cutoff:(max(dat3) + 1)), log10(gamma.trace3)[-1], lwd = 3, col = the
 lines(log10(cutoff:(max(dat4) + 1)), log10(gamma.trace4)[-1], lwd = 3, col = thepalOrder[4])
 legend("bottomleft",legend=c("Highly ordered","Some order", "Almost random", "Random"),pch=15,col=c(thepal[1:4]))
 dev.off()
+
+
+plot(log10(cutoff:(max(dat1) + 1)), log10(exp.trace1)[-1], lwd = 3, col = thepalOrder[1], type = "l")
+lines(log10(cutoff:(max(dat2) + 1)), log10(exp.trace2)[-1], lwd = 3, col = thepalOrder[2])
+lines(log10(cutoff:(max(dat3) + 1)), log10(exp.trace3)[-1], lwd = 3, col = thepalOrder[3])
+lines(log10(cutoff:(max(dat4) + 1)), log10(exp.trace4)[-1], lwd = 3, col = thepalOrder[4])
+legend("bottomleft",legend=c("Highly ordered","Some order", "Almost random", "Random"),pch=15,col=c(thepal[1:4]))
