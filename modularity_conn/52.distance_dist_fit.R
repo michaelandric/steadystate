51f1565e808e03a332b2f342ca6e2da827abaeea
Michael Andric <andric.michael@gmail.com>
2013-06-24 12:18:07 +0200

    get distribution parameters

---
+++ modularity_conn/52.distance_dist_fit.R
@@ -0,0 +1,16 @@
+# get parameters for distribution fit
+library(MASS)
+ss <- noquote(strsplit(Args," ")[[1]][1])
+conditions <- seq(4)
+
+dist_fit <- c()
+
+for (i in conditions)
+{
+    dist_name <- paste("/mnt/tier2/urihas/Andric/steadystate/",ss,"/corrTRIM_BLUR/distance_noavgs_Filter20_",ss,"_Cond",i,".txt",sep="")
+    dist_scores <- as.matrix(read.table(dist_name))
+    dist_scores <- dist_scores[!dist_scores < 20]   # filters final couple voxels that got 0 in original filter
+    dist_fit <- rbind(dist_fit, fitdistr(dist_scores, "gamma")$estimate)
+}
+
+write.table(dist_fit, paste("distance_gammafits_",ss,".txt",sep=""), row.names=F, quote=F)
