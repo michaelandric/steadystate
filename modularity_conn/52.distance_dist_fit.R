567e63c4330897f38b4464b07fb48026de096631
Michael Andric <andric.michael@gmail.com>
2013-06-24 12:23:01 +0200

    setwd

--- modularity_conn/52.distance_dist_fit.R
+++ modularity_conn/52.distance_dist_fit.R
@@ -2,12 +2,13 @@
 library(MASS)
 ss <- noquote(strsplit(Args," ")[[1]][1])
 conditions <- seq(4)
-
 dist_fit <- c()
+
+setwd(paste("/mnt/tier2/urihas/Andric/steadystate/",ss,"/corrTRIM_BLUR/", sep="")
 
 for (i in conditions)
 {
-    dist_name <- paste("/mnt/tier2/urihas/Andric/steadystate/",ss,"/corrTRIM_BLUR/distance_noavgs_Filter20_",ss,"_Cond",i,".txt",sep="")
+    dist_name <- paste("distance_noavgs_Filter20_",ss,"_Cond",i,".txt", sep="")
     dist_scores <- as.matrix(read.table(dist_name))
     dist_scores <- dist_scores[!dist_scores < 20]   # filters final couple voxels that got 0 in original filter
     dist_fit <- rbind(dist_fit, fitdistr(dist_scores, "gamma")$estimate)
