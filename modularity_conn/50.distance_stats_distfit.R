51c23594d131a74788546bc2ae601bcce34480bf
Michael Andric <andric.michael@gmail.com>
2013-06-27 12:35:10 +0200

    now does rate and shape estimate for xyz 20mm filtered

--- modularity_conn/50.distance_stats_distfit.R
+++ modularity_conn/50.distance_stats_distfit.R
@@ -1,28 +1,44 @@
 # Examine voxel-wise distances across conditions
 # Find whether shape parameter differs by condition across the group
+# Find whether rate parameter differs by condition across the group
 library(MASS)
+library(nortest)
 subjects <- c("ANGO","CLFR","MYTP","TRCO","PIGL","SNNW","LDMW","FLTM","EEPA","DNLN","CRFO","ANMS","MRZM","MRVV","MRMK","MRMC","MRAG","MNGO","LRVN")
 conditions <- seq(4)
-dist_fits <- c()
+
+gamma_dat <- c()
+rate_dat <- c()
+
 
 for (ss in subjects)
 {
     for (i in conditions)
     {
-        dist_scores <- paste("/mnt/tier2/urihas/Andric/steadystate/",ss,"/corrTRIM_BLUR/distance_",ss,"_Cond",i,".txt",sep="")
+        dist_scores <- paste("/mnt/tier2/urihas/Andric/steadystate/",ss,"/corrTRIM_BLUR/distance_xyz_Filter20_",ss,"_Cond",i,".txt", sep="")
         dist_dat <- as.matrix(read.table(dist_scores))
         dist_dat <- dist_dat[!dist_dat == 0]   # remove where distance did not have value pass filter
-        dist_fits <- c(dist_fits, fitdistr(dist_dat, "gamma")$estimate[[1]])   # this 'estimate' the gamma shape parameter
+        gamma_dat <- c(gamma_dat, fitdistr(dist_dat, "gamma")$estimate[[1]])   # this 'estimate' the gamma shape parameter
+        rate_dat <- c(rate_dat, fitdistr(dist_dat, "gamma")$estimate[[2]])   # this 'estimate' the gamma rate parameter
     }
 }
 
 setwd("/mnt/tier2/urihas/Andric/steadystate/groupstats/")
-dist_fits_mat <- matrix(c(dist_fits), ncol=4, byrow=T)
-dist_df <- data.frame(dist_fits, rep(seq(4), 19), rep(subjects, each=4))
-colnames(dist_df) <- c("dists", "condition", "subject")
-print(summary(aov(dists ~ condition + Error(subject/condition), data=dist_df)))
-print(dist_fits_mat)
-print(colMeans(dist_fits_mat))
-print(apply(dist_fits_mat, 2, median))
-print(shapiro.test(dist_fits_mat))
-print(friedman.test(dist_fits_mat))
+
+gamma_df <- data.frame(gamma_dat, as.factor(rep(seq(4), 19)), rep(subjects, each=4))
+rate_df <- data.frame(rate_dat, as.factor(rep(seq(4), 19)), rep(subjects, each=4))
+colnames(gamma_df) <- c("gamma", "condition", "subject")
+colnames(rate_df) <- c("rate", "condition", "subject")
+gamma_mat <- matrix(gamma_dat, ncol=4, byrow=T)
+rate_mat <- matrix(rate_dat, ncol=4, byrow=T)
+
+for (n in c("gamma", "rate"))
+{
+    dist_df <- get(paste(n,"_df", sep=""))
+    dist_mat <- get(paste(n,"_mat", sep=""))
+    print(aggregate(dist_df[,1], list(dist_df$condition), summary))
+    print(aggregate(dist_df[,1], list(dist_df$subject), summary))
+    print(ad.test(dist_df[,1]))   # normality test
+    print(summary(aov(dist_df[,1] ~ condition + Error(subject/condition), data=dist_df)))
+    print(friedman.test(dist_mat))
+}
+
