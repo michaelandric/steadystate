468eb4dfd4389fc8ef615af3ea794346a7508eb7
Michael Andric <andric.michael@gmail.com>
2013-06-27 17:20:38 +0200

    Do this now for whole distribution. More print statements

--- modularity_conn/54.dist_fitRsq.R
+++ modularity_conn/54.dist_fitRsq.R
@@ -23,7 +23,44 @@ for (ss in subjects)
 }
 
 setwd(paste("/mnt/tier2/urihas/Andric/steadystate/groupstats/", sep=""))
+print("=====For distributions with average at each voxel======")
+print(matrix(Rsq_vec, ncol=4, byrow=T))
+print(matrix(shape_param_vec, ncol=4, byrow=T))
+shape_df <- data.frame(shape_param_vec, as.factor(rep(seq(4), 19)), rep(subjects, each=4))
+Rsq_df <- data.frame(Rsq_vec, as.factor(rep(seq(4), 19)), rep(subjects, each=4))
+colnames(shape_df) <- c("shapes", "condition", "subject")
+colnames(Rsq_df) <- c("RsqVals", "condition", "subject")
 
+print(mean(Rsq_vec))
+print(aggregate(Rsq_df$RsqVals, list(Rsq_df$condition), summary))
+
+
+# Now for the large distributions (not average at each voxel)
+Rsq_vec <- c()
+shape_param_vec <- c()
+for (ss in subjects)
+{
+    setwd(paste("/mnt/tier2/urihas/Andric/steadystate/",ss,"/corrTRIM_BLUR/", sep=""))
+    dd <- read.table(paste("distance_gammafits_",ss,".txt", sep=""), header=T)
+
+    for (i in conditions)
+    {
+        print(date())
+        dist_dat <- as.matrix(read.table(paste("distance_noavgs_Filter20_",ss,"_Cond",i,".txt", sep="")))
+        dist_dat <- dist_dat[!dist_dat == 0]   # remove where distance did not have value pass filter
+        est_shape = dd$shape[i]
+        est_rate = dd$rate[i]
+        b = rgamma(length(dist_dat), shape=est_shape, rate=est_rate)
+        Rsq = summary(lm(sort(dist_dat) ~ sort(b)))$r.squared
+        Rsq_vec <- c(Rsq_vec, Rsq)
+        shape_param_vec <- c(shape_param_vec, shape)
+    }
+}
+
+setwd(paste("/mnt/tier2/urihas/Andric/steadystate/groupstats/", sep=""))
+print("=====For distributions without average at each voxel======")
+print(matrix(Rsq_vec, ncol=4, byrow=T))
+print(matrix(shape_param_vec, ncol=4, byrow=T))
 shape_df <- data.frame(shape_param_vec, as.factor(rep(seq(4), 19)), rep(subjects, each=4))
 Rsq_df <- data.frame(Rsq_vec, as.factor(rep(seq(4), 19)), rep(subjects, each=4))
 colnames(shape_df) <- c("shapes", "condition", "subject")
