15b76e543fd3f24346a21b059c5804a999a55b4b
michaelandric <andric.michael@gmail.com>
2013-07-09 14:14:52 +0200

    now with thresh variable

--- modularity_conn/RInvoke_blondel.py
+++ modularity_conn/RInvoke_blondel.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+"""
+This uses a different RInvoke because we want to keep the *Rout file. 
+So we copy the script as unique names to keep unique *Rout files.
+The Rout files show the number of hierarchical levels -- needed for the community organization at each level, done in next script
+"""
 import os
 import sys
 from subprocess import call
@@ -9,7 +14,8 @@ import shutil
 R_SCRIPT_PREF = sys.argv[1]
 ss = sys.argv[2]
 Cond = sys.argv[3]
-os.environ["R_ARGS"] = ss+" "+Cond
+Thresh = sys.argv[4]
+os.environ["R_ARGS"] = ss+" "+Cond+" "+Thresh
 
 print os.environ["R_ARGS"]
 print R_SCRIPT_PREF
@@ -17,7 +23,7 @@ print R_SCRIPT_PREF
 
 WORKDIR = os.environ["state"]+"/"+ss+"/corrTRIM_BLUR/"
 R_SCRIPT = R_SCRIPT_PREF+".R"
-R_SCRIPT_NEW = R_SCRIPT_PREF+"_gray_"+ss+Cond+".R"
+R_SCRIPT_NEW = R_SCRIPT_PREF+"_gray_"+ss+Cond+"_thresh_"+Thresh+".R"
 shutil.copy2(R_SCRIPT,WORKDIR+R_SCRIPT_NEW)
 os.chdir(WORKDIR)
 
