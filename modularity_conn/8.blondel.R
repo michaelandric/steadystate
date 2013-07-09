bf63a8ce90c9adfeaa7c71f29826d8f8873d9bd2
michaelandric <andric.michael@gmail.com>
2013-07-09 14:14:45 +0200

    now with thresh variable

--- modularity_conn/8.blondel.R
+++ modularity_conn/8.blondel.R
@@ -1,23 +1,17 @@
-library(bct)
-
+## This now includes 'thresh' variable 
 Args <- Sys.getenv("R_ARGS")
 print(noquote(strsplit(Args," ")[[1]]))
 print(length(noquote(strsplit(Args," ")[[1]])))
 subj <- noquote(strsplit(Args," ")[[1]][1])
 cond <- as.numeric(noquote(strsplit(Args," ")[[1]][2]))
+thresh <- as.numeric(noquote(strsplit(Args," ")[[1]][3]))
 
+library(bct)
+srcdst <- paste(Sys.getenv("state"),"/",subj,"/corrTRIM_BLUR/cleanTS.",cond,".",subj,"_graymask_dump.bin.corr.thresh_",thresh,".srcdst",sep="")
+tree <- paste(Sys.getenv("state"),"/",subj,"/corrTRIM_BLUR/cleanTS.",cond,".",subj,"_graymask_dump.bin.corr.thresh_",thresh,".tree",sep="")
+modularity_score = blondel_community(srcdst, tree)
+print(modularity_score)
 
-blondel_meth <- function(i,ss)
-{
-    library(bct)
-    srcdst <- paste(Sys.getenv("state"),"/",subj,"/corrTRIM_BLUR/cleanTS.",cond,".",subj,"_graymask_dump.bin.corr.thresh.srcdst",sep="")
-    tree <- paste(Sys.getenv("state"),"/",subj,"/corrTRIM_BLUR/cleanTS.",cond,".",subj,"_graymask_dump.bin.corr.thresh.tree",sep="")
-    modularity_score = blondel_community(srcdst,tree)
-    return(modularity_score)
-    print(modularity_score)
-}
+modout <- paste(Sys.getenv("state"),"/",subj,"/corrTRIM_BLUR/mod_score.",subj,".Cond",cond,"thresh_",thresh,".txt",sep="")
+write.table(modularity_score, modout, row.names=F, col.names=F, quote=F)
 
-mod = blondel_meth(cond,subj)
-print(mod)
-modout <- paste(Sys.getenv("state"),"/",subj,"/corrTRIM_BLUR/mod_score.",subj,".Cond",cond,".txt",sep="")
-write.table(mod,modout,row.names=F,col.names=F,quote=F)
