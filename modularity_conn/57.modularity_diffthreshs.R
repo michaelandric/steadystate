1ac026e28cc6ba83b763bb1815891190e65631ab
Michael Andric <andric.michael@gmail.com>
2013-07-12 14:15:40 +0200

    stats to look at modularity at diff thresholds

---
+++ modularity_conn/57.modularity_diffthreshs.R
@@ -0,0 +1,50 @@
+# assess the modularity values at different thresholds
+subjects <- c("ANGO","CLFR","MYTP","TRCO","PIGL","SNNW","LDMW","FLTM","EEPA","DNLN","CRFO","ANMS","MRZM","MRVV","MRMK","MRMC","MRAG","MNGO","LRVN")
+conditions <- seq(4)
+thresholds <- c("0.2", "0.3", "0.4", "0.5")
+
+library(RColorBrewer)
+thepal = colorRampPalette(brewer.pal(9,"Set2"))(9)
+
+
+dat <- c()
+for (t in 1:(length(thresholds)-1)) # 0.5 is the main data. It is under different name than the others so it's in next loop
+{
+    for (ss in subjects)
+    {
+        setwd(paste("/mnt/tier2/urihas/Andric/steadystate/",ss,"/corrTRIM_BLUR/", sep=""))
+        for (i in conditions)
+        {
+            dat <- c(dat, round(read.table(paste("mod_score.",ss,".Cond",i,"thresh_",thresholds[t],".txt", sep=""))[[1]], 4))
+        }
+    }
+}
+
+dat_orig <- c()
+for (ss in subjects)
+{
+    setwd(paste("/mnt/tier2/urihas/Andric/steadystate/",ss,"/corrTRIM_BLUR/", sep=""))
+    for (i in conditions)
+    {
+        dat_orig <- c(dat_orig, round(read.table(paste("mod_score.",ss,".Cond",i,".txt", sep=""))[[1]], 4))
+    }
+}
+
+setwd("/mnt/tier2/urihas/Andric/steadystate/groupstats/")
+
+dat <- c(dat, dat_orig)
+condition_vec <- rep(as.factor(rep(seq(4), 19)), 4)
+subjects_vec <- rep(rep(subjects, each=4), 4)
+thresh_levels <- rep(thresholds, each = length(subjects) * length(conditions))
+mod_score_frame <- data.frame(dat, condition_vec, subjects_vec, thresh_levels)
+colnames(mod_score_frame) <- c("modularity", "condition", "subject", "thresh")
+
+## Now analyze modularity variance at each correlation matrix threshold 
+for (t in levels(mod_score_frame$thresh))
+{
+    print(paste("===== At correlation matrix threshold: ",t," =====",sep=""))
+    tmp <- subset(mod_score_frame, thresh == t)
+    print(summary(aov(modularity ~ condition + Error(subject/condition), data=tmp)))
+    print(aggregate(tmp$modularity, list(tmp$condition), mean))
+}
+
