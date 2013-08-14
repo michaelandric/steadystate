92595a2a8af334d06d2597a9f0eaadd74d20e163
Michael Andric <andric.michael@gmail.com>
2013-08-14 02:28:20 +0200

    add background grid with barplot2

--- modularity_conn/58.modularity_bargraphs.R
+++ modularity_conn/58.modularity_bargraphs.R
@@ -1,10 +1,11 @@
 # Make modularity bar graphs with proper within-subject error bars
+# MUST BE RUN ON 30 OR 32 TO USE gplots library
+library(gplots)
+library(RColorBrewer)
+
 subjects <- c("ANGO","CLFR","MYTP","TRCO","PIGL","SNNW","LDMW","FLTM","EEPA","DNLN","CRFO","ANMS","MRZM","MRVV","MRMK","MRMC","MRAG","MNGO","LRVN")
 conditions <- seq(4)
 condition_names = c("Highly ordered", "Some order", "Random", "Almost Random")
-
-library(RColorBrewer)
-thepal = colorRampPalette(brewer.pal(9,"Blues"))(9)[c(7, 5, 3, 1)]
 
 dat_orig <- c()
 for (ss in subjects)
@@ -38,8 +39,10 @@ aa_ordered <- aa[c(2, 4, 1, 3)]
 error_vec_ordered <- error_vec[c(1, 2, 4, 3)]
 
 #bar.col <- grey.colors(length(levels(factor(list(mod_score_frame$condition)[[1]]))))  # alternative (removal of) colors
+thepal = colorRampPalette(brewer.pal(9,"Blues"))(9)[c(7, 5, 3, 1)]
 pdf("modularity_bargraph.pdf")
-ab <- barplot(aa_ordered, beside = TRUE, ylim = ylim, ylab = "Modularity (Q)", col = thepal)
+ab <- barplot2(aa_ordered, beside = TRUE, ylim = ylim, ylab = "Modularity (Q)", col = thepal, plot.grid = TRUE)
+#ab <- barplot(aa_ordered, beside = TRUE, ylim = ylim, ylab = "Modularity (Q)", col = thepal)   # use this if gplots not available
 segments(x0 = ab, x1 = ab, y0 = aa_ordered, y1 = aa_ordered + error_vec_ordered)
 segments(x0 = ab - .2, x1 = ab + .2, y0 = aa_ordered + error_vec_ordered, y1 = aa_ordered + error_vec_ordered)
 dev.off()
