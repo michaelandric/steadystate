af5ca18e35b17c5c0851ec850a3d2afbe0edf530
michaelandric <andric.michael@gmail.com>
2013-09-30 12:32:59 +0200

    added median output

--- modularity_conn/61.set_members_avg_preserved.R
+++ modularity_conn/61.set_members_avg_preserved.R
@@ -6,10 +6,13 @@ subj_mat <- matrix(ncol = length(subjects), nrow = 231203) ## these are nrows co
 for (i in 1:length(subjects))
 {
     setwd(paste("/mnt/tier2/urihas/Andric/steadystate/",subjects[i],"/corrTRIM_BLUR/", sep = ""))
-    subj_mat[,i] <- as.matrix(read.table(paste("preserved_",subjects[i],"_tlrc_dump.txt", sep = "")))[,4]
+    subj_mat[,i] <- as.matrix(read.table(paste("preserved_",subjects[i],"_warped_tlrc_dump.txt", sep = "")))[,4]
 }
 
 setwd("/mnt/tier2/urihas/Andric/steadystate/groupstats/")
 
-out_vec <- rowMeans(subj_mat)
-write.table(round(out_vec, 4), "preserved_group_avg.txt", row.names = F, col.names = F, quote = F)
+out_vec_avg <- rowMeans(subj_mat)
+out_vec_med <- apply(subj_mat, 1, median)
+write.table(round(out_vec_avg, 4), "preserved_warped_group_avg.txt", row.names = F, col.names = F, quote = F)
+write.table(round(out_vec_med, 4), "preserved_warped_group_median.txt", row.names = F, col.names = F, quote = F)
+
