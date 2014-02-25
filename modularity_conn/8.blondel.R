53d90eebe52cd67cbf53b54247ac2860c9091514
michaelandric <andric.michael@gmail.com>
2014-02-25 22:55:53 +0100

    linkthresh updates

--- modularity_conn/8.blondel.R
+++ modularity_conn/8.blondel.R
@@ -7,11 +7,11 @@ cond <- as.numeric(noquote(strsplit(Args," ")[[1]][2]))
 thresh <- as.numeric(noquote(strsplit(Args," ")[[1]][3]))
 
 library(bct)
-srcdst <- paste(Sys.getenv("state"),"/",subj,"/corrTRIM_BLUR/cleanTS.",cond,".",subj,"_graymask_dump.bin.corr.thresh_",thresh,".srcdst",sep="")
-tree <- paste(Sys.getenv("state"),"/",subj,"/corrTRIM_BLUR/cleanTS.",cond,".",subj,"_graymask_dump.bin.corr.thresh_",thresh,".tree",sep="")
+srcdst <- paste(Sys.getenv("state"),"/links_files/modularity/",subj,".",cond,".",thresh,"median_linksthresh.out.srcdst", sep="") 
+tree <- paste(Sys.getenv("state"),"/links_files/modularity/",subj,".",cond,".",thresh,"median_linksthresh.out.tree", sep="")
 modularity_score = blondel_community(srcdst, tree)
 print(modularity_score)
 
-modout <- paste(Sys.getenv("state"),"/",subj,"/corrTRIM_BLUR/mod_score.",subj,".Cond",cond,"thresh_",thresh,".txt",sep="")
+modout <- paste(Sys.getenv("state"),"/links_files/modularity/mod_score.",subj,".",cond,".",thresh,"median_linksthresh.txt",sep="")
 write.table(modularity_score, modout, row.names=F, col.names=F, quote=F)
 
