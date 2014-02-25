89fbcb1cbec7b883f2fdf6074dda653e90785820
michaelandric <andric.michael@gmail.com>
2014-02-25 16:21:12 +0100

    updated for linksthresh

--- modularity_conn/7.blondel_convert.R
+++ modularity_conn/7.blondel_convert.R
@@ -1,14 +1,14 @@
 ## take the bin and make into link format for the blondel method to find communities
 Args <- Sys.getenv("R_ARGS")
 print(noquote(strsplit(Args," ")[[1]]))
-print(length(noquote(strsplit(Args," ")[[1]])))
 subj <- noquote(strsplit(Args," ")[[1]][1])
 cond <- as.numeric(noquote(strsplit(Args," ")[[1]][2]))
 thresh <- as.numeric(noquote(strsplit(Args," ")[[1]][3]))
 
 library(bct)
-thresh_mat <- paste(Sys.getenv("state"),"/",subj,"/corrTRIM_BLUR/cleanTS.",cond,".",subj,"_graymask_dump.bin.corr.thresh_",thresh,sep="")
-blondel_mat <- paste(Sys.getenv("state"),"/",subj,"/corrTRIM_BLUR/cleanTS.",cond,".",subj,"_graymask_dump.bin.corr.thresh_",thresh,".srcdst",sep="")
-blondel_convert(thresh_mat,blondel_mat)
+
+thresh_mat <- paste(Sys.getenv("state"),"/links_files/",subj,".",cond,".",thresh,"median_linksthresh.out", sep="")
+blondel_mat <- paste(Sys.getenv("state"),"/links_files/",subj,".",cond,".",thresh,"median_linksthresh.out.srcdst", sep="")
+blondel_convert(thresh_mat, blondel_mat)
 
 
