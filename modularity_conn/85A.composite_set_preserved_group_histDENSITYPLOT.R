8a4a3feebd418815c9cec749e9476c2af22b4061
michaelandric <andric.michael@gmail.com>
2014-04-14 14:30:56 +0200

    doing density plot rather than straight histogram

---
+++ modularity_conn/85A.composite_set_preserved_group_histDENSITYPLOT.R
@@ -0,0 +1,20 @@
+## do a combined histogram
+setwd("/mnt/tier2/urihas/Andric/steadystate/groupstats/")
+gg1 <- read.table("preserved_group_median5p_20vxFltr_warped_median.txt")$V1
+gg2 <- read.table("iters_group_median5p_20vxFltr_warped_median.txt")$V1
+newgg1 <- gg1[which(gg1 > 0 & gg1 < 777)]
+newgg2 <- gg2[which(gg2 > 0 & gg2 < 777)]
+
+stck <- c(newgg1, newgg2)
+snsc_frame <- data.frame(stck, c(rep("observed", length(newgg1)), rep("null", length(newgg2))))
+colnames(snsc_frame) <- c("SNSC", "set")
+
+
+library(ggplot2)
+cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+#ggplot(snsc_frame, aes(SNSC, fill=set)) + geom_histogram(binwidth = .03) + scale_fill_manual(values = cbbPalette[c(1,3)])
+#ggplot(snsc_frame, aes(SNSC, fill=set)) + geom_histogram(binwidth = .03)+scale_fill_manual(values=c("grey", "black")) + theme(panel.background = theme_rect(fill='white', colour='black'))
+pdf("preserved_group_iters_hists_with20vxFltr_completeDensity.pdf", paper = "USr", width = 11)
+qplot(SNSC, data = snsc_frame, geom = "density", fill = set, position = "stack") + scale_fill_manual(values = c("grey","black")) + theme(panel.background = element_rect(fill = "white", colour = "black"))
+dev.off()
+
